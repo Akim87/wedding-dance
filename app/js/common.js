781e10fba93d285844d9cfb7c501731afb23647c
Akim87 <akim3496389@gmail.com>
2020-04-18 09:28:50 +0300

    secton hero complete

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,4 @@
-$(".header__menu-burger").click(function() {
+$(".header__menu-burger, .menu__item").click(function() {
 	$(".menu").toggleClass("menu--active");
 	$(this).toggleClass("header__menu-burger--active");
 });
