7b55ca63d540ae3e6e1b2617053724ee8c87f5d9
Akim87 <akim3496389@gmail.com>
2020-04-23 22:55:54 +0300

    alpha

--- app/js/common.js
+++ app/js/common.js
@@ -1,21 +1,60 @@
-$(".header__menu-burger, .menu__link, .menu__btn").click(function() {
-	$(".menu").toggleClass("menu--active");
-	$(".header__menu-burger").toggleClass("header__menu-burger--active");
-	$("body").toggleClass("fixed");
+/*============ HEADER MOBILE MENU ============*/
+
+$(".header__menu-burger, .menu__link, .menu__btn").click(function () {
+  $(".menu").toggleClass("menu--active");
+  $(".header__menu-burger").toggleClass("header__menu-burger--active");
+  $("body").toggleClass("fixed");
 });
 
-$(document).ready(function(){
-  $('.slider').slick({
-		mobileFirst: true,
-		arrows: false,
-		slidesToShow: 1.2,
-		infinite: false,
-		adaptiveHeight: true,
-		responsive: [
-			{
-				breakpoint: 740,
-				settings: "unslick",
-			},
-		],
+/*============ SECTION SERVICE SLIDER ============*/
+
+$(document).ready(function () {
+  $(".services__inner").slick({
+    mobileFirst: true,
+    arrows: false,
+    slidesToShow: 1.2,
+    infinite: false,
+    adaptiveHeight: true,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 740,
+        settings: "unslick",
+      },
+    ],
   });
+});
+
+/*============ SECTION COACHES SLIDER ============*/
+
+$(document).ready(function () {
+  $(".coaches__inner")
+    .slick({
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      adaptiveHeight: true,
+      infinite: false,
+      arrows: true,
+      appendArrows: $(".coaches__navigation"),
+      prevArrow: '<button type="button" class="icon icon-back"></button>',
+      nextArrow: '<button type="button" class="icon icon-forward"></button>',
+      responsive: [
+        {
+          breakpoint: 960,
+          settings: {
+            slidesToScroll: 1,
+            arrows: false,
+            slidesToShow: 1.2,
+            dots: true,
+          },
+        },
+      ],
+    })
+    .on("afterChange", function (event, slick, currentSlide) {
+      $(".coaches__counter").text(currentSlide + 2 + " из " + slick.slideCount);
+    });
+  var slick = $(".coaches__inner").slick("getSlick");
+  $(".coaches__counter").text(
+    slick.currentSlide + 2 + " из " + slick.slideCount
+  );
 });
